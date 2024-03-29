5fb1c3e501c2b92d83a18860a99aae7ec71cf47c
joshuadunning <56651505+joshuadunning@users.noreply.github.com>
2024-03-29 04:06:22 -0500

    cleanup

--- src/routes/api/health/index.ts
+++ src/routes/api/health/index.ts
@@ -2,13 +2,13 @@ import { Request, Response } from 'express'
 
 export const get = (req:Request, res:Response) => {
     res.removeHeader('X-Powered-By')
-    res.removeHeader('Date')
-    res.removeHeader('Keep-Alive')
-    res.removeHeader('Connection')
-    res.removeHeader('Content-Type')
-    res.removeHeader('Content-Length')
-    res.removeHeader('Transfer-Encoding')
-    res.removeHeader('Last-Modified')
+    res.setHeader('Date', '')
+    res.setHeader('Content-Type', '')
+    res.setHeader('Content-Length', '')
+    res.setHeader('Transfer-Encoding','')
+    res.setHeader('Last-Modified', '')
+    res.setHeader('c-do-app-origin', '')
     res.setHeader('Set-Cookie', '')
-    res.send('ok')
+    res.setHeader('Cache-Control', '')
+    res.end()
 }
