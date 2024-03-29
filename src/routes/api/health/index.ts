44390926afa7c4f0f3071fb419e85fce29381de0
joshuadunning <56651505+joshuadunning@users.noreply.github.com>
2024-03-29 11:13:16 -0500

    cleanup

--- src/routes/api/health/index.ts
+++ src/routes/api/health/index.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express'
+import dotenv from 'dotenv'
+dotenv.config({path:`.env.${process.env.NODE_ENV}`})
 
 export const get = (req:Request, res:Response) => {
     res.removeHeader('X-Powered-By')
     res.setHeader('Date', '')
     res.setHeader('Last-Modified', '')
+    console.warn(`TEST: ${process.env.TEST}`)
     res.end()
 }
