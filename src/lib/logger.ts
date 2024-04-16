c747484fe26c71c1e126c45459c49e5ea733d9a7
joshuadunning <56651505+joshuadunning@users.noreply.github.com>
2024-04-16 14:19:54 -0500

    2.3.3

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,5 +1,19 @@
 import pino from 'pino'
+import dotenv from 'dotenv'
+dotenv.config({path:`.env.${process.env.NODE_ENV}`})
 
+if(
+    process.env.NODE_ENV !== 'development'
+){
+    pino.transport({
+        targets:[
+            {
+                target: '@logtail/pino',
+                options: {sourceToken: process.env.LOGTAIL_SOURCE_TOKEN }
+            }
+        ]
+    })
+}
 const logger = pino.pino()
 
 export default logger
